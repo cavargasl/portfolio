6c70efff86a4b4a27162050fdd3fbfe01a4078a4
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-28 17:24:02 -0500

    Fix: static params dynamic

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,11 +1,12 @@
+import { i18n, type Locale } from "@/config/i18n-config"
 
 export function generateStaticParams() {
-  return [{ lang: 'en' }, { lang: 'es' }]
+  return i18n.locales.map((locale) => ({ lang: locale }))
 }
 
 interface RootProps {
   children: React.ReactNode
-  params: { lang: string }
+  params: { lang: Locale }
 }
 export default function Root({ children, params }: RootProps) {
   return (
