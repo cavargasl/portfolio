b6b993c1aa0a076f33ef5336cf1e8b3e897cabde
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-04 16:17:42 -0500

    update: provider i18n and implement to all page

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,8 +1,8 @@
 import SidebarNav from "@/app/[locale]/components/layouts/SidebarNav"
 import { TailwindIndicator } from "@/components/TailwindIndicator"
+import { I18nProvider } from "@/components/provider"
 import { getSiteConfig } from "@/config/siteConfig"
 import "@/styles/global.css"
-import I18nProvider from "./components/provider/I18nProvider"
 
 // TODO: generate metadata for the root
 interface RootProps {
@@ -13,12 +13,12 @@ export default async function Root({ children, params }: RootProps) {
   const { name, mainNav } = await getSiteConfig()
 
   return (
-    <html lang={params.locale}>
+    <html lang={params.locale} suppressHydrationWarning>
       <body>
-        <main className='p-4 sm:w-[calc(100%-16rem)]'>
-          {children}
-        </main>
         <I18nProvider locale={params.locale}>
+          <main className='p-4 sm:w-[calc(100%-16rem)]'>
+            {children}
+          </main>
           <SidebarNav siteConfig={{ name, mainNav }} />
         </I18nProvider>
         <TailwindIndicator />
