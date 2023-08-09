74013581cc281479209d66d15b4af0a70dcc6aa8
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-09 18:53:15 -0500

    refactor: custom hook screen size

--- src/hooks/useScreenSize.tsx
+++ src/hooks/useScreenSize.tsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from 'react'
 
+interface ScreenSize {
+  SM: boolean
+  MD: boolean
+}
+const breakpoints = {
+  SM: 640, // Valor del breakpoint "sm" en Tailwind (640px)
+  MD: 768, // Valor del breakpoint "md" en Tailwind (768px)
+}
 export default function useScreenSize() {
-  const [isScreenLargerThanSM, setIsScreenLargerThanSM] = useState(false)
-
+  const [isScreenLargerThan, setIsScreenLargerThan] = useState<ScreenSize>({ SM: false, MD: false })
+  
   useEffect(() => {
     const checkScreenSize = () => {
-      const isLargerThanSM = window.innerWidth >= 640; // Valor del breakpoint "sm" en Tailwind (640px)
-      setIsScreenLargerThanSM(isLargerThanSM);
+      const isLargerThanSM = window.innerWidth >= breakpoints.SM
+      const isLargerThanMD = window.innerWidth >= breakpoints.MD
+      setIsScreenLargerThan({ SM: isLargerThanSM, MD: isLargerThanMD })
     }
 
     checkScreenSize()
@@ -14,5 +23,5 @@ export default function useScreenSize() {
     return () => window.removeEventListener('resize', checkScreenSize)
   }, [])
 
-  return { isScreenLargerThanSM }
+  return isScreenLargerThan
 }
