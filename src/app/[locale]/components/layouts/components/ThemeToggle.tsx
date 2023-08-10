1dc73adaf2fae9292bf3cce3521e820ee33cfdeb
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-09 23:37:24 -0500

    update: change toggle theme component

--- src/app/[locale]/components/layouts/components/ThemeToggle.tsx
+++ src/app/[locale]/components/layouts/components/ThemeToggle.tsx
@@ -1,22 +1,26 @@
 "use client"
+import { IconMoon, IconSun } from "@/components/Icons"
 import { useScopedI18n } from "@/locale/client"
 import { useTheme } from "next-themes"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
   const scopedT = useScopedI18n("commons")
+
   return (
-    <div className="toggle-theme">
-      <label className="toggle-label">
-        <input
-          type="checkbox"
-          checked={theme === "light"}
-          className="toggle-checkbox"
-          onChange={() => setTheme(theme === "light" ? "dark" : "light")}
-          aria-label={scopedT("toggleTheme")}
-        />
-        <span className="toggle-slider"></span>
-      </label>
-    </div>
+    <button
+      className="relative"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      aria-label={scopedT("toggleTheme")}
+    >
+      <IconSun
+        className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+        aria-hidden="true"
+      />
+      <IconMoon
+        className="absolute left-0 top-0 h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+        aria-hidden="true"
+      />
+    </button>
   )
 }
