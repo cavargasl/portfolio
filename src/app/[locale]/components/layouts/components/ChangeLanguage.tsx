f7f5ae97d87c64ddada2aa6d80fe70e4bde5b05d
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-11 15:06:42 -0500

    Update: change icon flag to text

--- src/app/[locale]/components/layouts/components/ChangeLanguage.tsx
+++ src/app/[locale]/components/layouts/components/ChangeLanguage.tsx
@@ -1,35 +1,23 @@
-import { FlagCo, FlagUs } from '@/components/Icons'
+import { Button, buttonVariants } from '@/components/ui/Button'
 import { Select, SelectContent, SelectTrigger } from '@/components/ui/Select'
+import { cn } from '@/lib/utils'
 import { useChangeLocale, useCurrentLocale, useScopedI18n } from '@/locale/client'
-import { type ReactElement } from 'react'
 
 export function ChangeLanguage() {
   const locale = useCurrentLocale()
   const scopedT = useScopedI18n("commons")
-  const flagIcon: { [key in typeof locale]: ReactElement } = {
-    en: <FlagUs className='w-8' />,
-    es: <FlagCo className='w-8' />
-  }
 
-  const changeLocale = useChangeLocale({
-    basePath: "/test"
-  })
+  const changeLocale = useChangeLocale()
 
   return (
-    <div>
-      <Select>
-        <SelectTrigger aria-label={scopedT("openLanguageMenu")} className='bg-background'>
-          {flagIcon[locale]}
-        </SelectTrigger>
-        <SelectContent side="top" className='bg-background'>
-          <span className='cursor-pointer' aria-label={scopedT("changeToEnglish")} onClick={() => changeLocale("en")}>
-            <FlagUs className='w-8 py-1' />
-          </span>
-          <span className='cursor-pointer' aria-label={scopedT("changeToSpanish")} onClick={() => changeLocale("es")}>
-            <FlagCo className='w-8 py-1' />
-          </span>
-        </SelectContent>
-      </Select>
-    </div>
+    <Select>
+      <SelectTrigger aria-label={scopedT("openLanguageMenu")} className={cn(buttonVariants({ variant: 'ghost', size: 'icon' }), "font-semibold")}>
+        {locale.toLocaleUpperCase()}
+      </SelectTrigger>
+      <SelectContent side='top' className='bg-background p-0'>
+        <Button variant="ghost" size="icon" className='hover:bg-inherit' aria-label={scopedT("changeToEnglish")} onClick={() => changeLocale("en")} disabled={locale === "en"}>EN</Button>
+        <Button variant="ghost" size="icon" className='hover:bg-inherit' aria-label={scopedT("changeToSpanish")} onClick={() => changeLocale("es")} disabled={locale === "es"}>ES</Button>
+      </SelectContent>
+    </Select>
   )
 }
