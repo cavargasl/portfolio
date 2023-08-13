8744adf57c98ece8eae0369c982a469ac39fea35
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-13 01:48:26 -0500

    refactor: sidebar

--- src/app/[locale]/components/layouts/SidebarNav.tsx
+++ src/app/[locale]/components/layouts/SidebarNav.tsx
@@ -9,6 +9,7 @@ import Image from "next/image"
 import { useState } from "react"
 import { ChangeLanguage } from "./components/ChangeLanguage"
 import { ThemeToggle } from "./components/ThemeToggle"
+import { createPortal } from "react-dom"
 
 interface SidebarProps {
   siteConfig: { mainNav: SidebarNavItems, name: string }
@@ -23,36 +24,51 @@ export default function SidebarNav({ siteConfig }: SidebarProps) {
 
   return (
     <>
-      {
-        !isOpen &&
-        <Button onClick={toggleSidebar} variant="ghost" size="icon" className="fixed right-4 top-4 lg:hidden" aria-label={scopedT("openSidebarNav")}>
-          <IconMenu className="h-6 w-6" aria-hidden="true" />
-        </Button>
-      }
+      {createPortal(
+        <>
+          {
+            isScreenLargerThan.LG ? null :
+              <Button
+                onClick={toggleSidebar}
+                variant="ghost"
+                size="icon"
+                className="fixed right-4 top-4 z-50"
+                aria-label={isOpen ? scopedT("closeSidebarNav") : scopedT("openSidebarNav")}
+              >
+                {
+                  isOpen ?
+                    <IconClose className="h-6 w-6" aria-hidden="true" />
+                    :
+                    <IconMenu className="h-6 w-6" aria-hidden="true" />
+                }
+              </Button>
+          }
+        </>,
+        document.body
+      )}
 
       <aside
         className={cn(
-          "fixed right-0 top-0 z-50 flex h-screen w-full translate-x-0 flex-col items-center justify-center gap-8 overflow-y-auto bg-muted px-3 py-10 transition-transform duration-[250] lg:w-64",
-          !isOpen && "duration-200",
+          "fixed right-0 top-0 z-40 flex min-h-screen w-full translate-x-0 flex-col items-center justify-center gap-8 bg-muted px-4 py-8 transition-transform duration-[250ms] ease-out-circ lg:w-64",
+          !isOpen && "duration-200 ease-in-out-expo",
           isScreenLargerThan.LG ? "translate-x-0" : isOpen ? "translate-x-0" : "translate-x-full"
         )}
         aria-label={scopedT("sidebarNav")}
       >
-        <header className="mb-auto">
-          <Button onClick={toggleSidebar} variant="ghost" size="icon" className="fixed right-4 top-4 lg:hidden" aria-label={scopedT("closeSidebarNav")}>
-            <IconClose className="h-6 w-6" aria-hidden="true" />
-          </Button>
-        </header>
+        <span
+          className="mb-auto"
+          aria-hidden="true"
+        />
 
-        <main className="flex flex-col items-center justify-center gap-8">
-            <Image
-              src="/images/profile.webp"
-              alt={siteConfig.name}
-              priority
-              width={232}
-              height={232}
-              className="object-cover"
-            />
+        <main className="flex w-full flex-col items-center justify-center gap-8">
+          <Image
+            src="/images/profile.webp"
+            alt={siteConfig.name}
+            priority
+            width={232}
+            height={232}
+            className="object-cover"
+          />
           <nav className="w-full">
             <ul className="flex flex-col gap-1">
               {
