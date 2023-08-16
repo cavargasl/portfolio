d90e68d46c3eb83cf6e8642f26e3b516b48195a2
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-15 20:15:41 -0500

    feat: create Separator component

---
+++ src/components/ui/Separator.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import { Root } from "@radix-ui/react-separator"
+import { forwardRef } from "react"
+
+const Separator = forwardRef<
+  React.ElementRef<typeof Root>,
+  React.ComponentPropsWithoutRef<typeof Root>
+>(
+  (
+    { className, orientation = "horizontal", decorative = true, ...props },
+    ref
+  ) => (
+    <Root
+      ref={ref}
+      decorative={decorative}
+      orientation={orientation}
+      className={cn(
+        "shrink-0 bg-foreground",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separator.displayName = Root.displayName
+
+export { Separator }
+
