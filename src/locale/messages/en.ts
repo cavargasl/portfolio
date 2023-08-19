ce5af0fe299eb37c0edb977a0eb1bcbfc0a59cf1
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-19 13:34:14 -0500

    feat: add translate of experience

--- src/locale/messages/en.ts
+++ src/locale/messages/en.ts
@@ -3,6 +3,58 @@ export default {
     "welcome": "Welcome to my professional portfolio, i am Multimedia Engineer, i live in Colombia, i'm passionate about challenges and acquire knowledge that allows me to grow both professionally and personally. My goal is to add value to those around me.",
     "iAm": "Frontend Developer"
   },
+  "experience": {
+    amount: 4,
+    jobs: [
+      {
+        position: 'Fullstack Developer',
+        title: 'Big Safe',
+        date: 'jul. 2023 · 1 month',
+        descriptions: [
+          "E - Commerce of sales of printing services in Next 13 with its new functionalities.",
+          "Reusable components made with Tailwind, CVA and clsx frameworks.",
+          "User management with Clerk framework.",
+          "Global state with Redux toolkit for shopping cart.",
+          "Implementation of Firebase Database and Firebase Storage."
+        ]
+      },
+      {
+        position: 'Frontend Developer',
+        title: 'CloudAPPi',
+        date: 'apr. 2022 - jan. 2023 · 10 months',
+        descriptions: [
+          "Create web application in charge of selection processes of technology professionals.",
+          "User management using the Keycloack framework.",
+          "Consumption of REST API with React Query and Axios to the Django backend implementing adapters.",
+          "Figma designs converted to code using the pixel perfect concept.",
+          "Migration of React project to Next 12 to optimize SEO.",
+          "Teleworking using the SCRUM methodology."
+        ]
+      },
+      {
+        position: 'Fullstack Developer',
+        title: 'Degree project',
+        date: 'jul. 2021 - apr. 2022 · 10 months',
+        descriptions: [
+          "DEVELOPMENT OF A WEB APPLICATION FOR THE EVALUATION OF PROPOSALS AND FINAL REPORTS OF DEGREE MODALITIES AT THE SAN BUENAVENTURA CARTAGENA UNIVERSITY.",
+          "Identify the functional and non-functional requirements that the Web application must have to manage the evaluation process, applying SCRUM.",
+          "Design the application, using Figma and the UX / UI bases to make it intuitive and comfortable for use.",
+          "Implement the database using Firebase services.",
+          "Develop the frontend with React JS"
+        ]
+      },
+      {
+        position: 'Frontend Developer',
+        title: 'DexFreight',
+        date: 'jan. 2021 - jun. 2021 · 6 months',
+        descriptions: [
+          "Add new functionalities in React Native.",
+          "Migration of Redux functionalities to native React hooks that did not need to be maintained in a global state.",
+          "Resolve tasks of style adjustments with sass and bug fixes on the web."
+        ]
+      },
+    ]
+  },
   "contact": {
     title: "Looking for a new addition to your team?",
     content: "I am currently available to join a team or company.{br}If you want to know more about me or my work, or just want to say hello, contact me by any of the following options.",
--- src/locale/messages/es.ts
+++ src/locale/messages/es.ts
@@ -3,7 +3,59 @@ export default {
     "welcome": "Bienvenid@ a mi portafolio profesional, soy Ingeniero Multimedia, vivo en Colombia, me apasionan los desafíos y adquirir conocimientos que me permitan crecer tanto a nivel profesional como personal. Mi objetivo es aportar valor a los que me rodean.",
     "iAm": "Desarrollador Frontend"
   },
-  "contact":{
+  "experience": {
+    amount: 4,
+    jobs: [
+      {
+        position: 'Fullstack Developer',
+        title: 'Big Safe',
+        date: 'jul. 2023 · 1 mes',
+        descriptions: [
+          "E - Commerce de ventas de servicios de impresión en Next 13 con sus nuevas funcionalidades.",
+          "Componentes reutilizable hechos con Tailwind, CVA y clsx frameworks.",
+          "Manejo de usuarios con Clerk framework.",
+          "Estado global con Redux toolkit para carrito de compra.",
+          "Implementación de Firebase Database y Firebase Storage."
+        ]
+      },
+      {
+        position: 'Frontend Developer',
+        title: 'CloudAPPi',
+        date: 'abr. 2022 - ene. 2023 · 10 meses',
+        descriptions: [
+          "Crear aplicación web encargada de procesos de selección de profesionales de la tecnología.",
+          "Manejo de usuario utilizando la tecnología de Keycloack framework.",
+          "Consumo de REST API con React Query y Axios al backend de Django implementando adapters.",
+          "Diseños de Figma convertidos a código mediante el concepto pixel perfect.",
+          "Migración de proyecto de React a Next 12 para optimizar el SEO.",
+          "Teletrabajo mediante la metodología SCRUM."
+        ]
+      },
+      {
+        position: 'Fullstack Developer',
+        title: 'Proyecto de grado',
+        date: 'jul. 2021 - abr. 2022 · 10 meses',
+        descriptions: [
+          "DESARROLLO DE UN APLICATIVO WEB PARA LA EVALUACIÓN DE PROPUESTAS E INFORMES FINALES DE MODALIDADES DE GRADO EN LA UNIVERSIDAD SAN BUENAVENTURA CARTAGENA.",
+          "Identificar los requerimientos funcionales y no funcionales con los que debe contar la aplicación Web para gestionar el proceso de evaluación, aplicando SCRUM.",
+          "Diseñar la aplicación, utilizando Figma y las bases de UX / UI para hacerla intuitiva y cómoda para su uso.",
+          "Implementar la base de datos haciendo uso de los servicios de Firebase.",
+          "Desarrollar el frontend con React JS"
+        ]
+      },
+      {
+        position: 'Frontend Developer',
+        title: 'DexFreight',
+        date: 'ene. 2021 - jun. 2021 · 6 meses',
+        descriptions: [
+          "Agregar funcionalidades nuevas en React Native.",
+          "Migración de funcionalidades de Redux a hooks nativos de React que no eran necesario mantener en un estado global.",
+          "Resolver tareas de ajustes de estilos con sass y correcciones de bugs en la web."
+        ]
+      },
+    ]
+  },
+  "contact": {
     title: "¿Buscando una nueva incorporación a tu equipo?",
     content: "Actualmente estoy disponible para unirme a un equipo o empresa.{br}Si quieres saber más sobre mí o mi trabajo, o simplemente quieres saludar, contácteme por cualquiera de las siguientes opciones.",
     cv: "Curriculum"
