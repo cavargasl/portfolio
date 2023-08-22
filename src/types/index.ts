c9bfc59226babe160d7f535e79be2015d9616d86
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-22 15:41:07 -0500

    feat: create type of skills

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,5 @@ interface NavItem {
 }
 
 export type SidebarNavItem = NavItem
+
+export type TypeSkills = 'JavaScript' | 'TypeScript' | 'Next.js' | 'React.js' | 'HTML' | 'CSS' | 'Tailwind CSS' | 'Styled Components' | 'Material UI' | 'Chakra UI' | 'Radix' | 'Node.js' | 'Express.js' | 'Nest.js' | 'tRPC' | 'Drizzle ORM' | 'Prisma' | 'Firebase' | 'MongoDB' | 'MySQL' | 'React Query' | 'Redux' | 'Axios' | 'Git' | 'GitHub' | 'GitLab' | 'Vitest' | 'Jest' | 'React Testing Library' | 'Cypress' | 'Clerk' | 'Figma' | 'Postman' | 'Visual Studio Code' | 'Jira Software' | 'Slack' | 'Keycloak' | 'SCRUM' | 'SASS' | 'React Native'
