d48fb44e43228fe3c92c19983761fe8adce4a545
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-23 19:31:59 -0500

    feat: add skillNames

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,7 @@ interface NavItem {
 export type SidebarNavItem = NavItem
 
 const skillsNames = [
-  'JavaScript', 'TypeScript', 'Next.js', 'React.js', 'HTML', 'CSS', 'Tailwind CSS', 'Styled Components', 'Material UI', 'Chakra UI', 'Radix', 'Node.js', 'Express.js', 'Nest.js', 'tRPC', 'Drizzle ORM', 'Prisma', 'Firebase', 'MongoDB', 'MySQL', 'React Query', 'Redux', 'Axios', 'Git', 'GitHub', 'GitLab', 'Vitest', 'Jest', 'React Testing Library', 'Cypress', 'Clerk', 'Figma', 'Postman', 'Visual Studio Code', 'Jira Software', 'Slack', 'Keycloak', 'SCRUM', 'SASS', 'React Native', 'Zod', 'React Hook Form',
+  'JavaScript', 'TypeScript', 'Next.js', 'React.js', 'HTML', 'CSS', 'Tailwind CSS', 'Styled Components', 'Material UI', 'Chakra UI', 'Radix', 'Node.js', 'Express.js', 'Nest.js', 'tRPC', 'Drizzle ORM', 'Prisma', 'Firebase', 'MongoDB', 'MySQL', 'React Query', 'Redux', 'Axios', 'Git', 'GitHub', 'GitLab', 'Vitest', 'Jest', 'React Testing Library', 'Cypress', 'Clerk', 'Figma', 'Postman', 'Visual Studio Code', 'Jira Software', 'Slack', 'Keycloak', 'SCRUM', 'SASS', 'React Native', 'Zod', 'React Hook Form', 'Type ORM', 'Class Validator',
 ] as const
 
 export type SkillName = (typeof skillsNames)[number]
