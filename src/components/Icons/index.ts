4db285a78a7e44b1824abeafc231ebaa3e2f9858
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-23 19:31:46 -0500

    feat: add icon

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -4,6 +4,7 @@ export * from './ChevronDown';
 export * from './Close';
 export * from './ExternalLink';
 export * from './Html';
+export * from './Image';
 export * from './Menu';
 export * from './Moon';
 export * from './Sun';
