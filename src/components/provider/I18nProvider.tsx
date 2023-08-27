1cc8e9995ff5fa6a2ce56eb1ffb1c8c43d6940e4
Camilo Vargas <camilovargasingm@gmail.com>
2023-08-27 12:48:08 -0500

    update: unnecessary code

--- src/components/provider/I18nProvider.tsx
+++ src/components/provider/I18nProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { I18nProviderClient } from '@/locale/client'
 import es from '@/locale/messages/es'
-import { type ReactNode, type ReactElement } from 'react'
+import { type ReactNode } from 'react'
 
 export function I18nProvider({ children, locale }: { children: ReactNode, locale: string }) {
   return <I18nProviderClient locale={locale} fallbackLocale={es}>{children}</I18nProviderClient>
