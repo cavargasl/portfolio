c6730e5fc96f86e730ddd91e0a5114db8d2da795
Camilo Vargas <camilovargasingm@gmail.com>
2024-10-21 02:20:08 -0500

    feat: setting lint-staged in a multi-pakage

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-  '*.{js,mjs,cjs,ts,json,md,html,css,scss,tsx,jsx}': ['npm run format'],
-  '*.{js,mjs,cjs,ts,tsx,jsx}': ['npm run lint'],
+  '*.{js,mjs,cjs,ts,json,md}': ['prettier --write'],
+  '*.{js,mjs,cjs,ts}': ['eslint --fix'],
 }
