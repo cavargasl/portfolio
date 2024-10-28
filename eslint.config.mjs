8eb834facdab1b4bd96343497dcfc9246a7cfb68
Camilo Vargas <camilovargasingm@gmail.com>
2024-10-28 16:13:38 -0500

    feat: update configurations of package and linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,18 @@ export default [
   {
     rules: {
       '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports', fixStyle: 'inline-type-imports' }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          'args': 'all',
+          'argsIgnorePattern': '^_',
+          'caughtErrors': 'all',
+          'caughtErrorsIgnorePattern': '^_',
+          'destructuredArrayIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'ignoreRestSiblings': true,
+        },
+      ],
     },
   },
   {
