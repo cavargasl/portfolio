6a0674ac9f64b43601bb020f79e60d59ab233998
Camilo Vargas <camilovargasingm@gmail.com>
2024-10-30 16:45:23 -0500

    feat: relocated siteConfig const

---
+++ core/const/siteConfig.ts
@@ -0,0 +1,13 @@
+const BASE_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1:3000'
+
+export const siteConfig = {
+  name: 'Camilo Vargas | Frontend Developer',
+  description: 'Personal web portfolio, where you can find information about me, my skills, my work experience, and my projects.',
+  url: BASE_URL,
+  ogImage: `${BASE_URL}/images/og.png`,
+  links: {
+    twitter: 'https://twitter.com/cavargasl',
+    github: 'https://github.com/cavargasl',
+    LinkedIn: 'https://www.linkedin.com/in/cavargasl/',
+  },
+}
