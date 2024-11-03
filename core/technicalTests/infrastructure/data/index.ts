83d1af0d1db5e913efe35e65dc64a1e698107132
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-02 23:54:18 -0500

    fix: description of ProMarketing test

--- core/technicalTests/infrastructure/data/index.ts
+++ core/technicalTests/infrastructure/data/index.ts
@@ -4,7 +4,7 @@ const esTechnicalTests: TechnicalTest[] = [
   {
     title: 'Componente de pasos a paso',
     description:
-      'Prueba técnica que consta en realizar un componente de pasos a paso utilizando NextJs, Typescript y TailwindCSS que reciba por hijos un numero indefinido de pasos, dicho componente debe ser totalmente customizable.',
+      'Prueba técnica que consta en realizar un componente de pasos a paso utilizando NextJs, Typescript y TailwindCSS que reciba por hijos un numero indefinido de pasos, dicho componente debe ser totalmente customizable. Basado en Stepper componente de Material Tailwind {url}',
     testTime: 'Tiempo para la prueba: 3 días',
     github: ['https://github.com/cavargasl/promarketing-test'],
     webSite: 'https://promarketing-test.vercel.app/',
