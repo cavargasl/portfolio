61e78cee85675b8bac9c9274a0c59d1563a0698f
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-08 01:13:11 -0500

    fix: resolve  error when jest compile external modules .js

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,14 @@ import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'node',
   verbose: true,
+  preset: 'ts-jest',
   transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
+    '^.+.tsx?$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
   moduleNameMapper: {
     '^@core/(.*)$': '<rootDir>/core/$1',
   },
+  transformIgnorePatterns: ['/node_modules/(?!.*\\.js$)'],
 }
 export default config
