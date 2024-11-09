4cf4e3e4e5334ca814de4633d3c48e16c6527a80
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-09 02:35:56 -0500

    feat: delete instances according the new structure

--- core/projects/application/projectService.ts
+++ core/projects/application/projectService.ts
@@ -1,7 +1,13 @@
 import { type Language } from '@core/locale/domain'
 
+import { sortSkills } from '@core/skills/domain/sortSkills'
+
 import { type ProjectRepository } from '../domain/projectRepository'
 
 export const projectsServices = (repository: ProjectRepository): ProjectRepository => ({
-  getAll: (lang: Language) => repository.getAll(lang),
+  getAll: async (lang: Language) => {
+    const projects = await repository.getAll(lang)
+    const sortedSkills = projects.map((test) => ({ ...test, skills: sortSkills(test.skills) }))
+    return sortedSkills
+  },
 })
