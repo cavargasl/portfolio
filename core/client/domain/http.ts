0dfe427abaac0b77b4437bfac3f2587445886bde
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-13 16:33:56 -0500

    delete unnecessary client

--- core/client/domain/http.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export type Http = {
-  get: <T>(url: string, params?: Record<string, any>, config?: any) => Promise<T>
-  post: <T>(url: string, params?: Record<string, any>, config?: any) => Promise<T>
-  put: <T>(url: string, params?: Record<string, any>, config?: any) => Promise<T>
-  delete: <T>(url: string, params?: any, config?: any) => Promise<T>
-}
