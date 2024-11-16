87ab04c911f0d7a8d046cf0dd55b32dabeac0018
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-15 22:16:47 -0500

    feat: add support for react

--- infrastructure/views/astro/astro.config.mjs
+++ infrastructure/views/astro/astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react({include: ['**/react/*']})],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
