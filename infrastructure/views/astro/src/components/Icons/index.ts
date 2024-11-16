d9796ef54672915a250cc7e1efd9d29d8844ac48
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-16 00:41:52 -0500

    feat: add icons

---
+++ infrastructure/views/astro/src/components/Icons/index.ts
@@ -0,0 +1,8 @@
+export { default as ChevronDown } from './ChevronDown.astro';
+export { default as Close } from './Close.astro';
+export { default as ExternalLink } from './ExternalLink.astro';
+export { default as Image } from './Image.astro';
+export { default as Link } from './Link.astro';
+export { default as Menu } from './Menu.astro';
+export { default as Moon } from './Moon.astro';
+export { default as Sun } from './Sun.astro';
