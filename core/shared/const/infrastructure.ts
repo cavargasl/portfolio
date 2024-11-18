ae0818d26aa386fd3e6fe7e4ed481bb3e67eeefa
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-18 15:09:49 -0500

    feat: get url of environment

--- core/shared/const/infrastructure.ts
+++ core/shared/const/infrastructure.ts
@@ -1,7 +1,5 @@
 import { type Language } from '@core/locale/domain'
 
-export const BASE_URL: Readonly<string> = process.env.NEXT_PUBLIC_APP_URL || 'http://127.0.0.1:3000'
-
 export const DEFAULT_LANG: Readonly<Language> = 'es'
 
 export const curriculum: Readonly<Record<Language, string>> = {
