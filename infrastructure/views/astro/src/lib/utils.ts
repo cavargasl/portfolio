991f2323985a81cf13049d40f4d690283f4a00e1
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-19 00:19:08 -0500

    feat: add id to Section Container

--- infrastructure/views/astro/src/lib/utils.ts
+++ infrastructure/views/astro/src/lib/utils.ts
@@ -1,6 +1,48 @@
+import type { getI18N } from '@/i18n'
+import { siteConfig } from '@core/shared/const/siteConfig'
+import type { SidebarNavItem, IdsNav } from '@core/shared/domain/navBar'
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type SiteConfigExtended = typeof siteConfig & {
+  mainNav: Record<SidebarNavItem["id"], SidebarNavItem>
+}
+export const siteConfigExtended = (t:ReturnType<typeof getI18N>): SiteConfigExtended => ({
+  ...siteConfig,
+  name: t.siteConfig.title,
+  description: t.siteConfig.description,
+  mainNav: {
+    aboutMe: {
+      id: 'aboutMe',
+      title: t.siteConfig.sidebarNav.aboutMe,
+    },
+    skills: {
+      id: 'skills',
+      title: t.siteConfig.sidebarNav.skills,
+    },
+    experience: {
+      id: 'experience',
+      title: t.siteConfig.sidebarNav.experience,
+    },
+    projects: {
+      id: 'projects',
+      title: t.siteConfig.sidebarNav.projects,
+    },
+    technicalTest: {
+      id: 'technicalTest',
+      title: t.siteConfig.sidebarNav.technicalTest,
+    },
+    education: {
+      id: 'education',
+      title: t.siteConfig.sidebarNav.education,
+    },
+    contact: {
+      id: 'contact',
+      title: t.siteConfig.sidebarNav.contact,
+    },
+  }
+})
