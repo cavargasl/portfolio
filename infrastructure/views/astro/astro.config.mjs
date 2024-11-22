23f9d8306ddeed87d6a781d52570a5d677a6e902
Camilo Vargas <camilovargasingm@gmail.com>
2024-11-22 15:03:04 -0500

    fix: add vercel serverless for deploy

--- infrastructure/views/astro/astro.config.mjs
+++ infrastructure/views/astro/astro.config.mjs
@@ -3,12 +3,21 @@ import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react({include: ['**/react/*']})],
+
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
   },
+
   output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
